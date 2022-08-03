3d3a3089375932ae79a588a05d4c11592c029611
Rabiul <rabiul35-487@diu.edu.bd>
2022-08-03 21:21:26 +0600

    Python Input file

---
+++ input_value.py
@@ -0,0 +1,32 @@
+# input value 4x4 (16byte)
+#Input system: as like this table
+#  a1  |  a2  |  a3  |  a4
+#  b1  |  b2  |  b3  |  b4
+#  c1  |  c2  |  c3  |  c4
+#  d1  |  d2  |  d3  |  d4
+
+
+#Coulumn1
+a1 = 0xEA
+a2 = 0x04
+a3 = 0x65
+a4 = 0x85
+
+#Column2
+b1 = 0x83
+b2 = 0x45
+b3 = 0x5D
+b4 = 0x96
+
+#Column3
+c1 = 0x5C
+c2 = 0x33
+c3 = 0x98
+c4 = 0xB0
+
+#Column4
+d1 = 0xF0
+d2 = 0x2D
+d3 = 0xAD
+d4 = 0xC5
+
